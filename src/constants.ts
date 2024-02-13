3c61e04a73e387da7af49f8ed85f1cc5f13129a1
telesyk <telesyk89@gmail.com>
2024-02-13 17:31:32 +0200

    Update endpoint to local data.json

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,5 @@
-export const MOCK_ENDPOINT = 'http://localhost:3000/api/get-mock-data'
+export const MOCK_ENDPOINT =
+  'https://sample-landing-with-auth.vercel.app/api/get-mock-data'
 export const STORAGE_NAME = 'UserData'
 export const CURRENCY_SIGN = '$'
 export const ANNUAL_PAYMENT_TYPE = 'annual'
